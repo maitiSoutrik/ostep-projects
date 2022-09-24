a4487283120310ee60ab7089dc11cfad3edae398
Soutrik Maiti <soutrikmaiti.uta@gmail.com>
2022-09-23 20:14:45 -0400

    Adding files from original repo

---
+++ initial-utilities/wcat/wcat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE (1024)
+
+int main(int argc, char *argv[])
+{
+  if(argc != 1)
+    {
+      for(int i=1; i<argc; i++)
+	{
+	  FILE *fp = fopen(argv[i], "r");
+	  if(fp == NULL)
+	    {
+	      printf("wcat:cannot open file\n");
+	      exit(1);
+	    }
+	  char buffer[1024];
+	  while(fgets(buffer, BUFFER_SIZE, fp) != NULL)
+	    {
+	      printf("%s", buffer);
+	    }
+	  fclose(fp);
+	}
+    }
+   exit(0);
+}
